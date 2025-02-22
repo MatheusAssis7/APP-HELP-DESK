8f3c11e914af3a86fe5a8b7d04df43aadb423df5
Matheus Assis <matheusassis825@gmail.com>
2025-02-22 11:54:09 -0300

    registrando chamados

--- Aplicação/abrir_chamado.php
+++ Aplicação/abrir_chamado.php
@@ -41,15 +41,15 @@
               <div class="row">
                 <div class="col">
                   
-                  <form>
+                  <form action="registrar_chamado.php" method="post">
                     <div class="form-group">
                       <label>Título</label>
-                      <input type="text" class="form-control" placeholder="Título">
+                      <input name="Titulo" class="form-control" placeholder="Título">
                     </div>
                     
                     <div class="form-group">
                       <label>Categoria</label>
-                      <select class="form-control">
+                      <select name="Categoria" class="form-control">
                         <option>Criação Usuário</option>
                         <option>Impressora</option>
                         <option>Hardware</option>
@@ -60,7 +60,7 @@
                     
                     <div class="form-group">
                       <label>Descrição</label>
-                      <textarea class="form-control" rows="3"></textarea>
+                      <textarea name="Descrição" class="form-control" rows="3"></textarea>
                     </div>
 
                     <div class="row mt-5">
---
+++ Aplicação/registrar_chamado.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$titulo = str_replace('#', '-', $_POST)['Titulo'];
+$categoria = str_replace('#', '-', $_POST)['Categoria'];
+$desgrição = str_replace('#', '-', $_POST)['Descrição'];
+
+$texto = $titulo .'#'. $categoria .'#'. $desgrição . PHP_EOL;
+//abrindo chamado
+$arquivo = fopen('../incorporando_scripts/arquivo.hd', 'a');
+//escrevendo chamado
+fwrite($arquivo, $texto);
+//fechando chamado
+fclose($arquivo);
+//redirecionando chamado
+header('Location: ../Aplicação/abrir_chamado.php');
+
+?>
