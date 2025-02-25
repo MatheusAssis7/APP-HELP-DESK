99ad185365142efe840d80fa461f02333bbe4534
Matheus Assis <matheusassis825@gmail.com>
2025-02-24 23:45:48 -0300

    alteração de user

--- Aplicação/valida_login.php
+++ Aplicação/valida_login.php
@@ -3,14 +3,18 @@
 session_start();
 
 $usuario_autenticado = false;
+$usuario_id = null;
 $usuarios_app = array(
-    array('email' => 'adm@teste.com.br', 'senha' => 'pk6577'),
-    array('email' => 'user@1teste.com.br', 'senha' => 'A123456'),
+    array('id' => 1, 'email' => 'adm1@teste.com.br', 'senha' => 'pk6577'),
+    array('id' => 2, 'email' => 'adm@2teste.com.br', 'senha' => 'A123456'),
+    array('id' => 3, 'email' => 'adm3@teste.com.br', 'senha' => 'pk6577'),
+    array('id' => 4, 'email' => 'adm4@teste.com.br', 'senha' => 'pk6577'),
 );
 
 foreach ($usuarios_app as $user) {
     if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
         $usuario_autenticado = true;
+        $usuario_id = $user['id'];
         break; // Adiciona um break para sair do loop após encontrar o usuário
     }
 }
@@ -18,9 +22,10 @@ foreach ($usuarios_app as $user) {
 if ($usuario_autenticado) {
     echo 'Usuário validado com sucesso';
     $_SESSION['autenticado'] = 'sim ';
-    header('location: home.php');
+    $_SESSION['id'] = $usuario_id;
+    //header('location: home.php');
 } else {
     $_SESSION['autenticado'] = 'nâo';
-    header('location: index.php?login=erro');
+    //header('location: index.php?login=erro');
 }
 ?>
